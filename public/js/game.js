f481cfb4efbf4ade0bc6d5f4123ed3d4eabc6487
Roman <roman.ermeev.dev@gmail.com>
2020-02-06 14:19:29 -0500

    ground image was rendered into canvas; target's coorginates were initiated in random order

--- public/js/game.js
+++ public/js/game.js
@@ -4,15 +4,22 @@ const ctx = canvas.getContext('2d');
 const ground = new Image();
 ground.src = 'img/ground.png';
 
-const food = new Image();
-food.src = 'img/food.png';
+const foodImage = new Image();
+foodImage.src = 'img/food.png';
 
-let box = 32;
+const box = 32;
 
 let score = 0;
+
+let food = {
+    x: Math.floor(Math.random() * 17 + 1) * 32,
+    y: Math.floor(Math.random() * 15 + 1) * 32
+}
 
 const drawGame = () => {
     ctx.drawImage(ground, 0, 0);
 }
 
 let game = setInterval(drawGame, 100);
+
+console.log(Math.random());
