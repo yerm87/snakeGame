a2a347d5f1cf42e7190b5acd7afc28203a8ed102
Roman <roman.ermeev.dev@gmail.com>
2020-02-06 15:29:34 -0500

    listener of key down was set to move snake accross game field and increment it if first coordinates of first element equal to target's coordinates

--- public/js/game.js
+++ public/js/game.js
@@ -22,6 +22,22 @@ snake[0] = {
     y: 10 * box
 }
 
+let dir;
+
+const direction = (event) => {
+    if(event.keyCode === 37 && dir !== 'right'){
+        dir = 'left';
+    } else if(event.keyCode === 40 && dir !== 'down'){
+        dir = 'up';
+    } else if(event.keyCode === 39 && dir !== 'left'){
+        dir = 'right';
+    } else if(event.keyCode === 38 && dir !== 'up'){
+        dir = 'down';
+    }
+}
+
+document.addEventListener('keydown', direction);
+
 const drawGame = () => {
     ctx.drawImage(ground, 0, 0);
 
@@ -35,7 +51,46 @@ const drawGame = () => {
     ctx.fillStyle = 'white';
     ctx.font = '50px Arial';
     ctx.fillText(score, box*2.5, box*1.8);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(snakeX === food.x && snakeY === food.y){
+        score++;
+
+        food = {
+            x: Math.floor(Math.random() * 17 + 1) * 32,
+            y: Math.floor(Math.random() * 15 + 3) * 32
+        }
+    } else {
+        snake.pop();
+    }
+
+    if(dir === 'left'){
+        snakeX -= box;
+    }
+
+    if(dir === 'right'){
+        snakeX += box;
+    }
+
+    if(dir === 'up'){
+        snakeY += box;
+    }
+
+    if(dir === 'down'){
+        snakeY -= box;
+    }
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
 }
+
+drawGame();
 
 let game = setInterval(drawGame, 100);
 
