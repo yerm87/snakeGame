5e84e3a0d83f5217db015b562a19da0fd603ea27
Roman <roman.ermeev.dev@gmail.com>
2020-02-06 14:46:17 -0500

    score indicator was added to canvas

--- public/js/game.js
+++ public/js/game.js
@@ -13,7 +13,7 @@ let score = 0;
 
 let food = {
     x: Math.floor(Math.random() * 17 + 1) * 32,
-    y: Math.floor(Math.random() * 15 + 1) * 32
+    y: Math.floor(Math.random() * 15 + 3) * 32
 }
 
 let snake = [];
@@ -26,6 +26,15 @@ const drawGame = () => {
     ctx.drawImage(ground, 0, 0);
 
     ctx.drawImage(foodImage, food.x, food.y);
+
+    for(let i=0; i<snake.length; i++){
+        ctx.fillStyle = 'green';
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = 'white';
+    ctx.font = '50px Arial';
+    ctx.fillText(score, box*2.5, box*1.8);
 }
 
 let game = setInterval(drawGame, 100);
